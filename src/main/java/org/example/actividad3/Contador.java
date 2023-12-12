b736a0e437bcf74900350af3eac9668f79ff1ce4
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2023-12-12 17:01:36 +0100

    actividad 3

--- src/main/java/org/example/actividad3/Contador.java
+++ src/main/java/org/example/actividad3/Contador.java
@@ -5,13 +5,19 @@ public class Contador {
     //private static int cantidad = 0;
     private int cantidad = 0;
 
-
     public void incrementar(){
         cantidad++;
     }
 
     public void decrementar(){
         cantidad--;
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+    public void setCantidad(int cantidad){
+        this.cantidad=cantidad;
     }
 
     @Override
